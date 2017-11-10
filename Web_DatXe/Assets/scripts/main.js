a6bc28386f62a6f25dd18d6e3dd44fbd6603cd9d
Nguyễn Thanh Phi <nguyenthanhphi0401@gmail.com>
2017-11-10 23:30:16 +0700

    update app1

--- Web_DatXe/Assets/scripts/main.js
+++ Web_DatXe/Assets/scripts/main.js
@@ -1,1 +1,57 @@
 ﻿
+var vm = new Vue({
+    el: "#submitform",
+    data: {
+        name: "",
+        phone: "",
+        type: "1",
+        address: ""
+    },
+    methods: {
+        submitInfo: function () {
+            var self = this;
+            if (self.name == "" || self.address == "" || self.phone == "" || self.type == "") {
+                bootbox.alert("<span style='color:red;'>Có lỗi xảy ra. Vui lòng thử lại</span>");
+            }
+            else {
+                saveMessage(self.name, self.phone, self.type, self.address);
+                self.name = "";
+                self.phone = "";
+                self.type = "1";
+                self.address = "";
+            }
+
+        }
+    }
+});
+
+
+// thiết lập xác thực firebase with google
+var database = firebase.database();
+function checkSetup() {
+    if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+        window.alert('You have not configured and imported the Firebase SDK. ' +
+            'Make sure you go through the codelab setup instructions and make ' +
+            'sure you are running the codelab using `firebase serve`');
+    }
+};
+
+function saveMessage(a, b, c, d) {
+
+    this.customerRef = this.database.ref('customer');
+    this.customerRef.push({
+        customerName: a,
+        address: d,
+        type: c,
+        telephone: b
+    }).then(function () {
+        bootbox.alert("Thêm thành công");
+
+    }.bind(this)).catch(function (error) {
+        console.error('Error writing new message to Firebase Database', error);
+        bootbox.alert("<span style='color:red;'>Có lỗi xảy ra. Vui lòng thử lại</span>");
+    });
+
+
+
+};
