31f4d40822f8a269dd89c579cd9d69a0d275677a
Nguyễn Thanh Phi <nguyenthanhphi0401@gmail.com>
2017-11-11 00:40:20 +0700

    init app2

---
+++ Web_DinhVi/Assets/scripts/main.js
@@ -0,0 +1,29 @@
+﻿var geocoder;
+var map;
+function initialize() {
+    geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(10.8230989, 106.6296638);
+    var mapOptions = {
+        zoom: 10,
+        center: latlng
+    }
+
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+
+
+function codeAddress(address) {
+
+    geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status == 'OK') {
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+            });
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
