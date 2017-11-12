242afd07337941cbd1bc0221e31a837cce15c4b2
thang-PC\thang <dinhthang2307@gmail.com>
2017-11-12 23:27:44 +0700

    load customer from firebase

--- Web_DinhVi/Assets/scripts/main.js
+++ Web_DinhVi/Assets/scripts/main.js
@@ -1,6 +1,15 @@
 ﻿var geocoder;
 var map;
-function initialize() {
+     var database = firebase.database();
+checkSetup = function () {
+    if (!window.firebase || !(firebase.app instanceof Function) || !firebase.app().options) {
+        window.alert('You have not configured and imported the Firebase SDK. ' +
+            'Make sure you go through the codelab setup instructions and make ' +
+            'sure you are running the codelab using `firebase serve`');
+    }
+}
+
+initialize=function() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(10.8230989, 106.6296638);
     var mapOptions = {
@@ -9,8 +18,25 @@ function initialize() {
     }
 
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    loadUnlocatedCustomer();
 }
 
+loadUnlocatedCustomer=function() {
+    var textVal = document.getElementById('testVal');
+    var customerRef = database.ref('customer');
+    var i = 2;
+    customerRef.on("child_added", retVal => {
+        var status = retVal.child("status").val();
+        var address = retVal.child("address").val();
+        var customerName = retVal.child("customerName").val();
+        var telephone = retVal.child("telephone").val();
+        
+       if (status == "0") {
+           $("#selection").append("<option value=" + i + ">" + address + "</option>");
+           i++;
+        }
+    });
+}
 
 
 function codeAddress(address) {
