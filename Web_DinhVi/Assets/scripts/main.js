aea56dc8df92a3abc28cc6977d0bdfb91811045a
Nguyễn Thanh Phi <nguyenthanhphi0401@gmail.com>
2017-11-18 00:54:48 +0700

    app2 complete 90%

--- Web_DinhVi/Assets/scripts/main.js
+++ Web_DinhVi/Assets/scripts/main.js
@@ -3,6 +3,9 @@ var map;
 var markers = [];
 
 var motorbikes = [];
+var listfreemotorbikes = [];
+var nearestmotorbikes = [];
+
 var customers = [];
 var serviceDistance;
 
@@ -63,19 +66,18 @@ function loadDataMotorbike() {
 
 
 
-};
+}
 
 function saveDatabase(key, biensoxe, chuxe, diachi, kinhdo, vido, loaixe, status) {
-    //console.log(key + " " + biensoxe + " " + diachi + " " + kinhdo + " " + vido + " " + loaixe + " " + status);
-    //console.log("==============================");
 
-    var data = [biensoxe, chuxe, diachi, kinhdo, vido, loaixe, status];
+    var data = [key, biensoxe, chuxe, diachi, kinhdo, vido, loaixe, status];
     motorbikes.push(data);
 
 }
 
 
 var loadUnlocatedCustomer = function () {
+
     this.messagesRef = this.database.ref('customer');
 
     this.messagesRef.off();
@@ -90,14 +92,14 @@ var loadUnlocatedCustomer = function () {
 
     this.messagesRef.on('child_added', setMessage);
     this.messagesRef.on('child_changed', setMessage);
-    this.messagesRef.on('child_removed', setMessage);//không xét tới
+    //this.messagesRef.on('child_removed', setMessage);//không xét tới
 
 
 }
 
 function saveCustomerData(key, address, customerName, status, tel, type) {
     var data = [key, address, customerName, status, tel, type];
-  
+
     if (status == "0") {
         customers.push(data);
 
@@ -134,39 +136,6 @@ function loadListDinhVi() {
 }
 
 
-//ham xu ly su kien khi nguoi dung click nut xac dinh
-
-function submitAddress() {
-    //get selected text in dropdown list using javascript
-    var datalist = document.getElementById('selection');
-    document.getElementById('address').value = datalist.options[datalist.selectedIndex].text;
-    //update status field = 0
-
-    //khai bao reference
-    var customerRef = database.ref('customer');
-    var postData;
-    //lay ref child can thay doi
-    var childKey;
-    customerRef.on("child_added", retVal => {
-        var address = retVal.child("address").val();
-        if (address == datalist.options[datalist.selectedIndex].text) {
-            childKey = retVal.key;
-            //create post entity
-            postData = {
-                customerName: retVal.val().customerName,
-                address: retVal.val().address,
-                status: retVal.val().status,
-                type: retVal.val().type,
-                telephone: retVal.val().telephone,
-                status: "1"
-            };
-        }
-    });
-    //tien hanh update 
-    var childRef = customerRef.child(childKey);
-    childRef.update(postData);
-    loadUnlocatedCustomer();
-}
 
 function updateMarkerPosition(latLng) {
     //in ra vị trí trong quá trình drag
@@ -175,7 +144,7 @@ function updateMarkerPosition(latLng) {
 
 }
 function geocodePosition(pos) {
-   
+
     console.log(pos.lat());
 
     geocoder.geocode({
@@ -186,7 +155,7 @@ function geocodePosition(pos) {
             console.log(responses);
             $('#selection option:selected').text(responses[0].formatted_address);
             ////$('#selection option:selected').css('color', 'red');
-            //$('#selection').find('option:selected').css('color', 'red');
+            $('#selection').find('option:selected').css("background-color", "yellow");
             codeAddress(responses[0].formatted_address);
         } else {
             alert('Cannot determine address at this location.');
@@ -198,6 +167,7 @@ function codeAddress(address) {
 
 
     deleteMarkers();
+    listfreemotorbikes = [];
 
     geocoder.geocode({ 'address': address }, function (results, status) {
         if (status == 'OK') {
@@ -210,14 +180,14 @@ function codeAddress(address) {
             });
             map.setZoom(17);
             map.setCenter(markermain.position);
-           
+
             markers.push(markermain);
 
             google.maps.event.addListener(markermain, 'click', function () {
                 map.setZoom(17);
                 map.setCenter(markermain.getPosition());
             });
-         
+
 
             //thêm sự kiện drag
             google.maps.event.addListener(markermain, 'dragstart', function () {
@@ -259,21 +229,11 @@ function codeAddress(address) {
             for (var i = 0; i < motorbikes.length; i++) {
                 var motorbike = motorbikes[i];
 
-                //showw toan bo xe
-                //var marker = new google.maps.Marker({
-                //    position: { lat: parseFloat(motorbike[4]), lng: parseFloat(motorbike[3]) },
-                //    map: map,
-                //    icon: image,
-                //    shape: shape,
-                //    title: motorbike[1]
-                //    //zIndex: beach[3]
-                //});
 
-                //show 10 xe gan nhat: 300m-600m-1km
-
-                if (motorbike[6] == "1") {
-                    var a = new google.maps.LatLng(parseFloat(motorbike[4]), parseFloat(motorbike[3]));
+                if (motorbike[7] == "1") {
+                    var a = new google.maps.LatLng(parseFloat(motorbike[5]), parseFloat(motorbike[4]));
                     destination.push(a);
+                    listfreemotorbikes.push(motorbike);
                 }
 
 
@@ -304,22 +264,40 @@ function codeAddress(address) {
                     var destinations = response.destinationAddresses;
 
                     var results = response.rows[0].elements;
+                    var nearestDistance = 2000;
+
                     for (var j = 0; j < results.length; j++) {
                         var element = results[j];
                         var distance = element.distance.value;
 
+                        var address_ = destinations[j];
+
+                        //tim diem gan nhat
+                        if (distance < nearestDistance) {
+                            {
+                               
+                                nearestDistance = distance;
+                                if (nearestmotorbikes.length > 0)
+                                    nearestmotorbikes.pop();
+                                nearestmotorbikes.push(listfreemotorbikes[j]);
+                               
+                            }
+                        }
+
+
                         if (distance < 2000) {
                             geocoder.geocode({ 'address': destinations[j] }, function (results, status) {
                                 if (status == 'OK') {
-                                    map.setCenter(results[0].geometry.location);
+
                                     var marker = new google.maps.Marker({
                                         map: map,
                                         position: results[0].geometry.location,
                                         icon: image,
                                         shape: shape,
-                                        title: destinations[j]
+                                        title: address_
                                     });
                                     markers.push(marker);
+
 
                                 }
                                 else {
@@ -333,9 +311,10 @@ function codeAddress(address) {
                         setMapOnAll(markers);
 
                     }
-                }
-
+                }   
             }
+           
+
         }
         else {
             alert('Không tìm thấy !');
@@ -345,11 +324,62 @@ function codeAddress(address) {
     });
 }
 
+//dịnh vị
+function locatesleep(key) {
+    myVar = setTimeout(locate, 1300, key);
+}
+
+
 function locate(key)//định vị điểm và gửi cho xe
 {
-    console.log(motorbikes);
+  
 
-    // alert(key);
+    var customerRef = database.ref('customer');
+    var postData;
+    //lay ref child can thay doi
+    customerRef.on("child_added", retVal => {
+        var childKey = retVal.key;
+        if (key == childKey) {
+            //create post entity
+            postData = {
+                customerName: retVal.val().customerName,
+                address: retVal.val().address,
+                type: retVal.val().type,
+                telephone: retVal.val().telephone,
+                status: "1"
+            };
+        }
+    });
+
+    try {
+        //tien hanh update customer (status =1 đã đc định vị)
+        var childRef = customerRef.child(key);
+        childRef.update(postData);
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+
+
+    //gui thông tin và chọn xe
+    if (nearestmotorbikes.length == 0 || nearestmotorbikes == null) {
+        //không có xe nào
+        pushgrabinfo(key, "", Date.now(), 1);
+
+
+    }
+    else {
+        
+        console.log(nearestmotorbikes);
+        pushgrabinfo(key, nearestmotorbikes[0][0], Date.now(), 1);
+
+    }
+
+    nearestmotorbikes = [];
+   
+    deleteMarkers();
+
 
 }
 
@@ -367,4 +397,22 @@ function clearMarkers() {
 function deleteMarkers() {
     clearMarkers();
     markers = [];
+}
+
+function pushgrabinfo(customer, motorbike, date, status) {
+    this.customerRef = this.database.ref('grabinfo');
+    this.customerRef.push({
+        customer: customer,
+        date: date,
+        motorbike: motorbike,
+        status: "1"
+
+    }).then(function () {
+        bootbox.alert("Định vị thành công");
+
+    }.bind(this)).catch(function (error) {
+        console.error('Error writing new message to Firebase Database', error);
+        bootbox.alert("<span style='color:red;'>Có lỗi xảy ra. Vui lòng thử lại</span>");
+    });
+
 }
