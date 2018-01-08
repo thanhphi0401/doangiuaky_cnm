77fbf1a019072663641e87df1a94af5027754de6
thang-PC\thang <dinhthang2307@gmail.com>
2018-01-08 22:40:51 +0700

    app2+app4

---
+++ Web_DinhVi/webpack.config.js
@@ -0,0 +1,67 @@
+﻿"use strict";
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    entry: "./Assets/main.js",
+    output: {
+        path: path.join(__dirname, "./dist"),
+        filename: "build.js"
+    },
+    devServer: {
+        contentBase: ".",
+        host: "localhost",
+        port: 9000
+    },
+    plugins: [
+        // ...
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: '"production"'
+            }
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader"
+            },
+             {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            }, {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]'
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    devServer: {
+        historyApiFallback: true,
+        noInfo: true,
+        overlay: true
+    },
+    performance: {
+        hints: false
+    }
+};
